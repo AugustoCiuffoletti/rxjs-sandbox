5f39c87071950ea3b1673e9dfa97c2c5edfb945e
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2020-03-12 10:46:47 +0100

    Rimossa installazione locale di npm

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
-echo "Necessari i diritti di superuser per"
-echo "installare npm, gestione moduli per Node.js"
-
-sudo apt install npm
-npm init -y
+# sudo apt install npm # Solo se non è già installato
+# npm init -y # Idem
 npm install rxjs webpack webpack-dev-server typescript ts-loader
 npm install webpack-cli --save-dev
 
 echo "Modificare il file in src/index.ts con il codice da provare"
 echo "Lanciare il server con il comando 'npm run start'"
 echo "Vedere il risultato sul browser all'URL 'http://localhost:8080'"
+
